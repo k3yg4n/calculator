c77cd1b7ce1e1041158c697cf76d291baec2e6af
Keegan Liu <keeganliu5678@gmail.com>
2022-03-29 23:00:18 -0400

    Fixed updateDisplay function's logic.

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ function divide(a,b){
 }
 
 function operate(op, num1, num2){
+    num1 = Number(num1);
+    num2 = Number(num2);
     if(op === "+"){return add(num1,num2);}
     if(op === "-"){return subtract(num1,num2);}
     if(op === multSymbol){return multiply(num1,num2);}
@@ -24,37 +26,84 @@ function operate(op, num1, num2){
 }
 
 // Button Event Listener Function
+// function updateDisplay(e){
+//     console.log("updateDisplay was run");
+//     if(e.target.id === "clrBtn"){ // Clear button is pressed
+//         display.textContent = "";
+//     } else if (e.target.id === "eqBtn"){ // Equal button is pressed
+        
+//         if(display.textContent.includes("+")){
+//             op = "+";
+//         } else if (display.textContent.includes("-")){
+//             op = "-";
+//         } else if (display.textContent.includes(multSymbol)){
+//             op = multSymbol;
+//         } else if (display.textContent.includes(divSymbol)){
+//             op = divSymbol
+//         }
+
+//         [num1,num2] = display.textContent.split(op);
+
+//         display.textContent = operate(op, num1, num2);
+//     } else { // Numerical Button is pressed or Operator is pressed
+//         display.textContent += e.target.textContent;
+//     }
+// }
+
+// function updateVars(e){
+//     console.log("updateVars was run");
+//     const value = e.target.textContent;
+//     if(e.target.className === "numBtn"){
+//         (num1 === null ? num1 = value : num2 = value);
+//     } else { // An operator button has been pressed
+//         op = e.target.textContent;
+//     }
+// }
+
 function updateDisplay(e){
-    console.log("updateDisplay was run");
-    if(e.target.id === "clrBtn"){ // Clear button is pressed
-        display.textContent = "";
-    } else if (e.target.id === "eqBtn"){ // Equal button is pressed
-        if(display.textContent.includes("+")){
-            op = "+";
-        } else if (display.textContent.includes("-")){
-            op = "-";
-        } else if (display.textContent.includes(multSymbol)){
-            op = multSymbol;
-        } else if (display.textContent.includes(divSymbol)){
-            op = divSymbol
+    let textToDisplay = "";
+
+    if(e.target.id === "clrBtn") { // if clear button is pressed
+        textToDisplay = ""; 
+        num1 = null;
+        num2 = null;
+        op = null;
+    } else if(e.target.className === "opBtn"){ // if operator button is pressed
+            let currOp = e.target.textContent;
+        if(num1 === null){ // the first value has not been received
+            console.log("FIRST VALUE WAS JUST RECEIVED!")
+            num1 = display.textContent;
+            op = currOp;
+            textToDisplay = num1 + op;
+        } else if(num2 === null && op !== null) {// the second value is displayed but not read yet
+            console.log("SECOND VALUE WAS JUST RECEIVED!")
+            num2 = getAfterOp(display.textContent);
+            textToDisplay = operate(op,num1,num2) + currOp;
+
+            num1 = operate(op,num1,num2);
+            op = currOp;
+            num2 = null;
+        } else if(num2 === null && op === null) { // num2 and op are unknown (after using equal button)
+            op = currOp;
+            textToDisplay = display.textContent + op;
         }
 
-        [num1,num2] = display.textContent.split(op);
-
-        display.textContent = operate(op, num1, num2);
-    } else if (e.target) { // Numerical value is pressed or Operator is pressed
-        display.textContent += e.target.textContent;
+    } else if(e.target.id === "eqBtn"){ // at this point we should have num1 and op
+        console.log("Equal button pressed.")
+        num2 = getAfterOp(display.textContent);
+        textToDisplay = operate(op, num1, num2);
+        num1 = textToDisplay;
+        op = null;
+        num2 = null;
+    } else { // if numerical btn is pressed
+        textToDisplay = (display.textContent + e.target.textContent);
     }
+    
+    display.textContent = textToDisplay;
 }
 
-function updateVars(e){
-    console.log("updateVars was run");
-    const value = e.target.textContent;
-    if(e.target.className === "numBtn"){
-        (num1 === null ? num1 = value : num2 = value);
-    } else { // An operator button has been pressed
-        op = e.target.textContent;
-    }
+function getAfterOp(displayText){
+    return displayText.split(String(op))[1];
 }
 
 ///// MAIN /////
@@ -75,13 +124,6 @@ for(let btn of buttons){
     btn.addEventListener('click',updateDisplay);
 }
 
-for (let btn of opButtons){
-    btn.addEventListener('click',updateVars);
-}
-
-for (let btn of numButtons){
-    btn.addEventListener('click',updateVars);
-}
 
 // Global variables
 let num1 = null;
